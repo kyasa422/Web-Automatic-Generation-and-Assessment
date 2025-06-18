3883e2a4564c90be775f7680e5a1227a56160746
Eugene702 <eugenefeilianputrarangga@gmail.com>
2025-06-19 00:11:50 +0700

    Bug

--- resources/js/Pages/Guru/Rekap/Edit.jsx
+++ resources/js/Pages/Guru/Rekap/Edit.jsx
@@ -86,7 +86,7 @@ const Edit = () => {
                 <input
                   type="checkbox"
                   value={permission.id}
-                  defaultChecked={data.permissions.includes(Number(permission.id))}
+                  defaultChecked={data.permissions.find(e => e.id == parseInt(permission)) != null}
                   onChange={handlePermissionChange}
                 />
                 <span>{permission.name}</span>
